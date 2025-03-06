e179c6b7292ce6ee8dcd0d230433738dd090f4a8
RinkaKesh <keshrinka2013@gmail.com>
2025-03-06 17:40:48 +0530

    server updated

--- Server/index.js
+++ Server/index.js
@@ -54,7 +54,7 @@ app.use(session({
   passport.use(new GoogleStrategy({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/user/auth/google/callback", 
+      callbackURL: "https://ark-note.vercel.app/user/auth/google/callback",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
     async (accessToken, refreshToken, profile, done) => {
